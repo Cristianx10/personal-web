3605df306d5bf98e2ace10564e91cee1069bac07
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-05-04 15:58:09 -0500

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/?', function(req, res) {
+app.get('/:var?', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
