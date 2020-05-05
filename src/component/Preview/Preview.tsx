086e99e8c8df5e1f1fb7552180e08acfa5d330f3
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-05-04 21:10:44 -0500

    preview

--- src/component/Preview/Preview.tsx
+++ src/component/Preview/Preview.tsx
@@ -9,7 +9,7 @@ const Preview = (props: IPreview) => {
     return (
         <div>
             <div style={{ left: "0", width: "100%", height: "0", position: "relative", paddingBottom: "141.4214%" }}>
-                <iframe src={props.url}
+                <iframe src={props.url + "?iframe=1"}
                     style={{ border: "0", top: "0", left: "0", width: "100%", height: "100%", position: 'absolute' }}
                     allowFullScreen></iframe>
             </div>
